edd2fdcfae61ee1275697b6549ff499aca289eec
Алекс Ш <slayer1649@gmail.com>
2023-07-27 18:20:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,57 +7,55 @@ SIZE = 40
 BACKGROUND_COLOR = (0, 0, 0)
 
 
-class Snake:
+class Skull:
     def __init__(self, parent_screen):
         self.parent_screen = parent_screen
-        self.image = pygame.image.load("resources/snakehead.jpg").convert()
-        self.image2 = pygame.image.load("resources/snakebody.jpg").convert()
-        self.direction = 'right'
-
+        self.image = pygame.image.load("resources/skull.png").convert()
+        self.image2 = pygame.image.load("resources/skullbody.png").convert()
         self.length = 1
-        self.x = [400]
-        self.y = [720]
-
-    def move_left(self):
-        if self.direction != 'right':
-            self.direction = 'left'
-
-    def move_right(self):
-        if self.direction != 'left':
-            self.direction = 'right'
-
-    def move_up(self):
-        if self.direction != 'down':
-            self.direction = 'up'
-
-    def move_down(self):
-        if self.direction != 'up':
-            self.direction = 'down'
-
-    def walk(self):
-        for i in range(self.length - 1, 0, -1):
-            self.x[i] = self.x[i - 1]
-            self.y[i] = self.y[i - 1]
-
-        if self.direction == 'left':
-            self.x[0] -= SIZE
-        if self.direction == 'right':
-            self.x[0] += SIZE
-        if self.direction == 'up':
-            self.y[0] -= SIZE
-        if self.direction == 'down':
-            self.y[0] += SIZE
-
-        self.draw()
+        self.x = [760]
+        self.y = [120]
+        self.direction = 'left'
 
     def draw(self):
-        self.parent_screen.fill(BACKGROUND_COLOR)
-        for i in range(self.length):
-            if i == 0:
-                self.parent_screen.blit(self.image, (self.x[i], self.y[i]))
-            else:
+        if self.length <= 1:
+            for i in range(self.length):
+                self.parent_screen.blit(self.image, (self.x[0], self.y[0]))
+
+        if self.length > 1:
+            for i in range(self.length):
+                self.parent_screen.blit(self.image, (self.x[0], self.y[0]))
                 self.parent_screen.blit(self.image2, (self.x[i], self.y[i]))
         pygame.display.flip()
+
+    def move(self):
+        for i in range(self.length):
+            self.y[i] = random.randint(1, 19) * SIZE
+            self.x[i] = 920
+
+    def walk(self):
+        # update body
+        if self.length <= 1:
+            for i in range(self.length - 1, 0, -1):
+                self.x[0] = self.x[i - 1]
+                self.y[0] = self.y[i - 1]
+
+        if self.length > 1:
+            for i in range(self.length - 1, 0, -1):
+                self.x[i] = self.x[i - 1]
+                self.y[i] = self.y[i - 1]
+
+        # update head
+        if self.direction == 'left':
+            self.x[0] -= SIZE / 2
+        if self.direction == 'right':
+            self.x[0] += SIZE / 2
+        if self.direction == 'up':
+            self.y[0] -= SIZE / 2
+        if self.direction == 'down':
+            self.y[0] += SIZE / 2
+
+        self.draw()
 
     def increase_length(self):
         self.length += 1
@@ -65,34 +63,9 @@ class Snake:
         self.y.append(-1)
 
     def decrease_length(self):
-        if self.length > 1:
-            self.length -= 1
-            self.x.pop()
-            self.y.pop()
-
-    def is_collision_with_skull(self, skull):
-        for i in range(1, skull.length):
-            if is_collision(self.x[0], self.y[0], skull.x[i], skull.y[i]):
-                return True
-        return False
-
-    def is_collision_with_bird(self, bird):
-        for i in range(1, bird.length):
-            if is_collision(self.x[0], self.y[0], bird.x[i], bird.y[i]):
-                return True
-        return False
-
-    def is_collision_with_apple(self, apple):
-        for i in range(apple.length):
-            if is_collision(self.x[0], self.y[0], apple.x, apple.y):
-                return True
-        return False
-
-    def is_collision_with_apple2(self, apple2):
-        for i in range(apple2.length):
-            if is_collision(self.x[0], self.y[0], apple2.x, apple2.y):
-                return True
-        return False
+        self.length -= 1
+        self.x.append(-1)
+        self.y.append(-1)
 
 
 class Apple:
@@ -105,7 +78,8 @@ class Apple:
         self.direction = 'right'
 
     def draw(self):
-        for _ in range(self.length):
+
+        for i in range(self.length):
             self.parent_screen.blit(self.image, (self.x, self.y))
 
     def move(self):
@@ -113,10 +87,12 @@ class Apple:
         self.x = 40
 
     def walk(self):
+        # update body
         for i in range(self.length - 1, 0, -1):
             self.x = self.x[i - 1]
             self.y = self.y[i - 1]
 
+        # update head
         if self.direction == 'left':
             self.x -= SIZE / 4
         if self.direction == 'right':
@@ -139,7 +115,8 @@ class Apple2:
         self.direction = 'up'
 
     def draw(self):
-        for _ in range(self.length):
+
+        for i in range(self.length):
             self.parent_screen.blit(self.image, (self.x, self.y))
 
     def move(self):
@@ -147,10 +124,12 @@ class Apple2:
         self.x = random.randint(1, 24) * SIZE
 
     def walk(self):
+        # update body
         for i in range(self.length - 1, 0, -1):
             self.x = self.x[i - 1]
             self.y = self.y[i - 1]
 
+        # update head
         if self.direction == 'left':
             self.x -= SIZE / 4
         if self.direction == 'right':
@@ -173,7 +152,8 @@ class Bird:
         self.direction = 'down'
 
     def draw(self):
-        for _ in range(self.length):
+
+        for i in range(self.length):
             self.parent_screen.blit(self.image, (self.x, self.y))
 
     def move(self):
@@ -181,10 +161,12 @@ class Bird:
         self.y = 40
 
     def walk(self):
+        # update body
         for i in range(self.length - 1, 0, -1):
             self.x = self.x[i - 1]
             self.y = self.y[i - 1]
 
+        # update head
         if self.direction == 'left':
             self.x -= SIZE / 4
         if self.direction == 'right':
@@ -197,44 +179,68 @@ class Bird:
         self.draw()
 
 
-class Skull:
+class Snake:
     def __init__(self, parent_screen):
         self.parent_screen = parent_screen
-        self.image = pygame.image.load("resources/skull.png").convert()
-        self.image2 = pygame.image.load("resources/skullbody.png").convert()
+        self.image = pygame.image.load("resources/snakehead.jpg").convert()
+        self.image2 = pygame.image.load("resources/snakebody.jpg").convert()
+        self.direction = 'right'
+
         self.length = 1
-        self.x = [760]
-        self.y = [120]
+        self.x = [400]
+        self.y = [720]
+
+    def move_left(self):
         self.direction = 'left'
 
-    def draw(self):
-        for i in range(self.length):
-            if i == 0:
-                self.parent_screen.blit(self.image, (self.x[i], self.y[i]))
-            else:
-                self.parent_screen.blit(self.image2, (self.x[i], self.y[i]))
-        pygame.display.flip()
+    def move_right(self):
+        self.direction = 'right'
 
-    def move(self):
-        for i in range(self.length):
-            self.y[i] = random.randint(1, 19) * SIZE
-            self.x[i] = 920
+    def move_up(self):
+        self.direction = 'up'
+
+    def move_down(self):
+        self.direction = 'down'
 
     def walk(self):
+        # update body
         for i in range(self.length - 1, 0, -1):
             self.x[i] = self.x[i - 1]
             self.y[i] = self.y[i - 1]
 
+        # update head
         if self.direction == 'left':
-            self.x[0] -= SIZE / 2
+            self.x[0] -= SIZE
         if self.direction == 'right':
-            self.x[0] += SIZE / 2
+            self.x[0] += SIZE
         if self.direction == 'up':
-            self.y[0] -= SIZE / 2
+            self.y[0] -= SIZE
         if self.direction == 'down':
-            self.y[0] += SIZE / 2
+            self.y[0] += SIZE
 
         self.draw()
+
+    def draw(self):
+        self.parent_screen.fill(BACKGROUND_COLOR)
+        if self.length <= 1:
+            for i in range(self.length):
+                self.parent_screen.blit(self.image, (self.x[0], self.y[0]))
+
+        if self.length > 1:
+            for i in range(self.length):
+                self.parent_screen.blit(self.image, (self.x[0], self.y[0]))
+                self.parent_screen.blit(self.image2, (self.x[i], self.y[i]))
+        pygame.display.flip()
+
+    def increase_length(self):
+        self.length += 1
+        self.x.append(-1)
+        self.y.append(-1)
+
+    def decrease_length(self):
+        self.length -= 1
+        self.x.append(-1)
+        self.y.append(-1)
 
 
 def is_collision(x1, y1, x2, y2):
@@ -247,14 +253,35 @@ def is_collision(x1, y1, x2, y2):
 class Game:
     def __init__(self):
         pygame.init()
-        pygame.display.set_caption("Snake And Apple Game")
+        pygame.display.set_caption(" Snake And Apple Game")
+
+        pygame.mixer.init()
+        # self.play_background_music()
+
         self.surface = pygame.display.set_mode((1000, 800))
-        self.clock = pygame.time.Clock()
         self.snake = Snake(self.surface)
+        self.snake.draw()
         self.apple = Apple(self.surface)
+        self.apple.draw()
         self.apple2 = Apple2(self.surface)
+        self.apple2.draw()
         self.bird = Bird(self.surface)
+        self.bird.draw()
         self.skull = Skull(self.surface)
+        self.skull.draw()
+
+    # def play_background_music(self):
+    #   pygame.mixer.music.load('resources/bg_music_1.mp3')
+    #  pygame.mixer.music.play(-1, 0)
+
+    # def play_sound(self, sound_name):
+    #  if sound_name == "crash":
+    #     sound = pygame.mixer.Sound("resources/crash.mp3")
+    # elif sound_name == 'ding':
+    #   sound = pygame.mixer.Sound("resources/ding.mp3")
+
+    # pygame.mixer.Sound.play(sound)
+    # pygame.mixer.music.stop()
 
     def reset(self):
         self.snake = Snake(self.surface)
@@ -266,6 +293,101 @@ class Game:
     def render_background(self):
         bg = pygame.image.load("resources/background.jpg")
         self.surface.blit(bg, (0, 0))
+
+    def play(self):
+        self.render_background()
+        self.snake.walk()
+        self.apple.walk()
+        self.apple2.walk()
+        self.bird.walk()
+        self.skull.walk()
+        self.display_score()
+        pygame.display.flip()
+
+        # snake eating apple scenario
+        if is_collision(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
+            self.snake.decrease_length()
+            self.skull.decrease_length()
+            self.apple.move()
+
+        # snake eating apple2 scenario
+        if is_collision(self.snake.x[0], self.snake.y[0], self.apple2.x, self.apple2.y):
+            self.snake.decrease_length()
+            self.skull.decrease_length()
+            self.apple2.move()
+
+        # apple colliding with SNAKE BODY
+        for i in range(2, self.snake.length):
+            if is_collision(self.apple.x, self.apple.y, self.snake.x[i], self.snake.y[i]):
+                self.snake.decrease_length()
+                self.skull.decrease_length()
+                self.apple.move()
+
+        # apple2 colliding with SNAKE BODY
+        for i in range(2, self.snake.length):
+            if is_collision(self.apple2.x, self.apple2.y, self.snake.x[i], self.snake.y[i]):
+                self.snake.decrease_length()
+                self.skull.decrease_length()
+                self.apple2.move()
+
+        # snake eating skull scenario
+        if is_collision(self.snake.x[0], self.snake.y[0], self.skull.x[0], self.skull.y[0]):
+            for i in range(self.snake.length):
+                self.snake.decrease_length()
+
+        # skull colliding with SNAKE BODY
+        for i in range(2, self.snake.length):
+            if is_collision(self.skull.x[0], self.skull.y[0], self.snake.x[i], self.snake.y[i]):
+                for ii in range(self.snake.length):
+                    self.snake.decrease_length()
+
+        # Skull body colliding with snake head
+        for i in range(2, self.skull.length):
+            if is_collision(self.skull.x[i], self.skull.y[i], self.snake.x[0], self.snake.y[0]):
+                for ii in range(self.snake.length):
+                    self.snake.decrease_length()
+
+        # snake eating bird scenario
+        if is_collision(self.snake.x[0], self.snake.y[0], self.bird.x, self.bird.y):
+            self.snake.increase_length()
+            self.bird.move()
+
+        # bird colliding with SNAKE BODY
+        for i in range(2, self.snake.length):
+            if is_collision(self.bird.x, self.bird.y, self.snake.x[i], self.snake.y[i]):
+                self.snake.increase_length()
+                self.bird.move()
+
+        # snake colliding with itself
+        # for i in range(2, self.snake.length):
+        #   if self.is_collision(self.snake.x[0], self.snake.y[0], self.snake.x[i], self.snake.y[i]):
+        #      # self.play_sound('crash')
+        #     raise "Collision Occurred"
+
+        if self.snake.length == 0:
+            raise "You are dead!"
+
+        # snake colliding with the boundries of the window
+        if not (0 <= self.snake.x[0] <= 1000 and 0 <= self.snake.y[0] <= 800):
+            # self.play_sound('crash')
+            raise "Hit the boundry error"
+
+        # bird colliding with the boundries of the window
+        if not (0 <= self.bird.x <= 1000 and 0 <= self.bird.y <= 800):
+            self.bird.move()
+
+        # apple colliding with the boundries of the window
+        if not (0 <= self.apple.x <= 1000 and 0 <= self.apple.y <= 800):
+            self.apple.move()
+
+        # apple colliding with the boundries of the window
+        if not (0 <= self.apple2.x <= 1000 and 0 <= self.apple2.y <= 800):
+            self.apple2.move()
+
+        # skull colliding with the boundries of the window
+        if not (0 <= self.skull.x[0] <= 1000 and 0 <= self.skull.y[0] <= 800):
+            self.skull.move()
+            self.skull.increase_length()
 
     def display_score(self):
         font = pygame.font.SysFont('arial', 30)
@@ -282,46 +404,6 @@ class Game:
         pygame.mixer.music.pause()
         pygame.display.flip()
 
-    def play(self):
-        self.render_background()
-        self.snake.walk()
-        self.apple.walk()
-        self.apple2.walk()
-        self.bird.walk()
-        self.skull.walk()
-        self.display_score()
-        pygame.display.flip()
-
-        if self.snake.is_collision_with_apple(self.apple):
-            self.snake.increase_length()
-            self.apple.move()
-
-        if self.snake.is_collision_with_apple2(self.apple2):
-            self.snake.increase_length()
-            self.apple2.move()
-
-        if self.snake.is_collision_with_skull(self.skull):
-            self.snake.decrease_length()
-
-        if self.snake.is_collision_with_bird(self.bird):
-            self.snake.increase_length()
-            self.bird.move()
-
-        if self.snake.length == 0:
-            raise Exception("You are dead!")
-
-        if not (0 <= self.snake.x[0] < 1000 and 0 <= self.snake.y[0] < 800):
-            raise Exception("Hit the boundary error")
-
-        if not (0 <= self.bird.x < 1000 and 0 <= self.bird.y < 800):
-            self.bird.move()
-
-        if not (0 <= self.apple.x < 1000 and 0 <= self.apple.y < 800):
-            self.apple.move()
-
-        if not (0 <= self.apple2.x < 1000 and 0 <= self.apple2.y < 800):
-            self.apple2.move()
-
     def run(self):
         running = True
         pause = False
@@ -331,32 +413,37 @@ class Game:
                 if event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
                         running = False
+
                     if event.key == K_RETURN:
                         pygame.mixer.music.unpause()
                         pause = False
+
                     if not pause:
                         if event.key == K_LEFT:
                             self.snake.move_left()
+
                         if event.key == K_RIGHT:
                             self.snake.move_right()
+
                         if event.key == K_UP:
                             self.snake.move_up()
+
                         if event.key == K_DOWN:
                             self.snake.move_down()
+
                 elif event.type == QUIT:
                     running = False
-
             try:
+
                 if not pause:
                     self.play()
+
             except Exception as e:
                 self.show_game_over()
                 pause = True
                 self.reset()
 
-            self.clock.tick(10)  # Set the game's frame rate
-
-        pygame.quit()
+            time.sleep(.1)
 
 
 if __name__ == '__main__':
